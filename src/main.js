2cbadf096c360754709a3985f4014c5ef1d2e56d
SimoneCalzari <simone.calzari@outlook.it>
2024-02-15 14:18:15 +0100

    feat: milestone 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 // file di partenza styles progetto
 import "./assets/scss/general.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 // file start Vue
 import App from "./App.vue";
 
